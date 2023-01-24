5de2a2247a351c0027987490a137658bb25a6794
DaviGGA <davigregorio.a@hotmail.com>
2023-01-24 18:42:34 -0300

    Game and Review models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class Game(models.Model):
+    game_id = models.IntegerField(primary_key=True)
+
+
+class Review(models.Model):
+    game_id = models.ForeignKey(Game,on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    comment =  models.TextField(blank=True,null=True)
+    grade = models.IntegerField()
